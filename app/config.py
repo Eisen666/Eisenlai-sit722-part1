b2a823f884f637a7e1a6bb7c50585d7a0d783b0a
Eisen666 <eisenlai321@gmail.com>
2024-08-14 15:24:56 +1000

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Settings:
-     DATABASE_URL: str = os.getenv("postgresql://sit722_part2_pkca_user:bJrNcboTxU39r0036DKW2QDF5d4HwOoo@dpg-cqu38o3v2p9s73cvuehg-a.oregon-postgres.render.com/sit722_part2_pkca")
+     DATABASE_URL: str = os.getenv("DATABASE_URL")
 
 settings = Settings()
